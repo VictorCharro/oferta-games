92ab7039290aeb90db3d36ebd9856dbb5c764ded
VcsDevs <v_chs@hotmail.com>
2026-07-14 22:19:14 -0300

    migra Supabase para Sao Paulo

--- frontend/src/app/services/supabase.ts
+++ frontend/src/app/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://klfqsadfbhjpiwozuezk.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsZnFzYWRmYmhqcGl3b3p1ZXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTkzMzcsImV4cCI6MjA5ODIzNTMzN30.ubKkKjBYwmRNbdgvkLDhvHgMF1f4B-Z8VTo6CsOdSJs'
+  'https://gxukrzmiloqdmtcgigqm.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4dWtyem1pbG9xZG10Y2dpZ3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNjUyOTIsImV4cCI6MjA5OTY0MTI5Mn0.0UZjhMClTtSQb5_XjC5dRwauqGtXrE48HMMSKbDnA30'
 );
